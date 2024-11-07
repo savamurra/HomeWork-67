b1e5eed9e9d76f9c6b2d5a91a831b53055303503
Sanzhar Beishekeev <franki-55@mail.ru>
2024-11-07 13:53:25 +0600

    Создал slice где написана логика для ввдение пароля, удаления и проверки

---
+++ src/containers/Keyboard/keyboardSlice.ts
@@ -0,0 +1,45 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+interface KeyboardState {
+    value: string;
+    background: string;
+    message: string;
+}
+
+const initialState: KeyboardState = {
+    value: '',
+    background: 'white',
+    message: '',
+};
+
+const correctPassword = '1345';
+
+export const keyboardSlice = createSlice({
+    name: 'keyboard',
+    initialState,
+    reducers: {
+        addPassword: (state, action) => {
+            if (state.value.length < 4) {
+                state.value += action.payload;
+            }
+        },
+
+        removePassword: (state) => {
+            state.value = state.value.slice(0, -1);
+        },
+
+        checkCorrectPassword: (state) => {
+            if (state.value === correctPassword) {
+                state.background = 'green';
+                state.message = 'Access Granted';
+            } else {
+                state.message = 'Access Denied';
+                state.background = 'red';
+            }
+        }
+    }
+});
+
+export const keyboardReducer = keyboardSlice.reducer;
+
+export const { addPassword, removePassword , checkCorrectPassword} = keyboardSlice.actions;
